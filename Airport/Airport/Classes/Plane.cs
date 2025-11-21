219b9ccced34578475b19ce6f44c1f12df46b8f3
DinaGladan <dina.gladan@gmail.com>
2025-11-21 10:51:22 +0100

    Added first case: view all planes

---
+++ Airport/Airport/Classes/Plane.cs
@@ -0,0 +1,26 @@
+﻿namespace Airport.Classes
+{
+    public class Plane
+    {
+        private string Id { get; set; }
+        public string Name { get; set; }
+        public int ProductionYear { get; set; }
+        public int NumberOfFlights { get; set; }
+
+        public Plane(string name, int productionYear, int numberOfFlights)
+        {
+            Id = Helper.generateID();
+            Name = name;
+            ProductionYear = productionYear;
+            NumberOfFlights = numberOfFlights;
+        }
+
+        public void printPlane()
+        {
+            Console.WriteLine($"{Id} - {Name} - {ProductionYear} - {NumberOfFlights}");
+        }
+
+        
+    }
+   
+}
